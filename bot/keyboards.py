509b5176b4c7f5ddc44f1828f1218c20999ff992
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-12 23:57:19 +0500

    move keyboard definition to separate file

---
+++ bot/keyboards.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def build_menu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardMarkup()
+    button_1 = KeyboardButton("Список товаров")
+    keyboard.add(button_1)
+    button_2 = KeyboardButton("Товар")
+    keyboard.add(button_2)
+    button_3 = KeyboardButton("Парсинг")
+    keyboard.add(button_3)
+    button_4 = KeyboardButton("Скачать")
+    keyboard.add(button_4)
+    return keyboard
